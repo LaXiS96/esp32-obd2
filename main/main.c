be2c83a3de145dc1df33086c495fbf9dde0eed3b
Antonio Ceccato <antonio.ceccato@hotmail.it>
2020-08-30 18:57:59 +0200

    Added WiFi SoftAP

--- main/main.c
+++ main/main.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+
+#include "wifi.c"
 
 void app_main(void)
 {
-    int i = 0;
-    while (1)
-    {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
     }
+    ESP_ERROR_CHECK(ret);
+
+    wifi_init();
 }
---
+++ main/wifi.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include "esp_log.h"
+#include "esp_wifi.h"
+
+#define AP_WIFI_SSID "ESP32 OBD-II"
+#define AP_WIFI_PASSWORD "esp32obd"
+#define AP_WIFI_CHANNEL 6
+#define AP_MAX_STA_CONN 4
+
+static const char *TAG = "wifi_ap";
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED)
+    {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " joined, AID=%d", MAC2STR(event->mac), event->aid);
+    }
+    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
+    {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " left, AID=%d", MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = AP_WIFI_SSID,
+            .ssid_len = strlen(AP_WIFI_SSID),
+            .channel = AP_WIFI_CHANNEL,
+            .password = AP_WIFI_PASSWORD,
+            .max_connection = AP_MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init finished. SSID=\"%s\" password=\"%s\" channel=%d", AP_WIFI_SSID, AP_WIFI_PASSWORD, AP_WIFI_CHANNEL);
+}
